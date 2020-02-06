a92e2fa424bc04f757f98a7f56ae9b9e70b5130d
Sheshank-Srivastava <ambitious9350164795@gmail.com>
2020-02-06 14:06:53 +0530

    Setting userlist adapter (progress)

--- app/src/main/java/com/monnfamily/aptwitterclone/adapter/UserListAdapter.java
+++ app/src/main/java/com/monnfamily/aptwitterclone/adapter/UserListAdapter.java
@@ -1,4 +1,48 @@
 package com.monnfamily.aptwitterclone.adapter;
 
-public class UserListAdapter {
+import android.content.Context;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+public class UserListAdapter extends RecyclerView.Adapter<UserListAdapter.UserListItem> {
+    Context context;
+    ArrayList<String> userList;
+    ClickListener clickListener;
+
+    public UserListAdapter(Context context, ArrayList<String> userList, ClickListener clickListener) {
+        this.context = context;
+        this.userList = userList;
+        this.clickListener = clickListener;
+    }
+
+    @NonNull
+    @Override
+    public UserListItem onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull UserListItem holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+    public class UserListItem extends RecyclerView.ViewHolder{
+        public UserListItem(@NonNull View itemView) {
+            super(itemView);
+        }
+    }
+    public interface ClickListener{
+        void onClickItem(View v);
+    }
 }
